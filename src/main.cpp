eec49f8cd9057533eb7867c51f468f57544584ba
Arman <110356055+Winlogon-exe@users.noreply.github.com>
2023-06-14 18:52:48 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,32 @@
 #include <iostream>
-#include<thread>
-#include"Client.h"
-#include"Server.h"
-
+#include <thread>
+#include "Client.h"
+#include "Server.h"
 
 int main() {
     setlocale(LC_ALL, "RUS");
-    boost::asio::io_context io_context;
-    boost::asio::io_context io_context2;
 
-    //usually ports should be different but if build on 1 PC ,need to use 1 port
-    uint16_t port_server = 8080;
+    boost::asio::io_context server_io_context;
+    boost::asio::io_context client_io_context;
 
-    
-    boost::asio::streambuf buffer_server;
-    auto server = std::make_shared<Server>(io_context, port_server, buffer_server);
+    uint16_t server_port = 8080;
 
-   
-    auto client = std::make_shared<Client>(io_context2, port_server, buffer_server);
+    // Создаем объект сервера и передаем io_context и порт в конструктор
+    boost::asio::streambuf server_buffer;
+    auto server = std::make_shared<Server>(server_io_context, server_port, server_buffer);
+
+    // Создаем объект клиента и передаем io_context и порт в конструктор
+    auto client = std::make_shared<Client>(client_io_context, server_port, server_buffer);
+
     server->Listening();
-    std::this_thread::sleep_for(std::chrono::seconds(1));
     client->ConnectToServer();
 
-    
-    std::thread server_thread([&]() { io_context.run(); });
-
-    std::thread client_thread([&]() {io_context2.run(); });
+    // Запускаем io_context для сервера и клиента
+    std::thread server_thread([&]() { server_io_context.run(); });
+    std::thread client_thread([&]() { client_io_context.run(); });
 
     server_thread.join();
     client_thread.join();
-    
+
     return 0;
 }
